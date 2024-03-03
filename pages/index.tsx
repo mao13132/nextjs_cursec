18a35f789a0fd24dd5a03fbf873ed645ed3fe5ed
mao13132 <mao13132@gmail.com>
2024-03-03 17:43:36 +0300

    add component P component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { Button, Htag } from "@/components";
+import { Button, Htag, P } from "@/components";
 
 export default function Home() {
   return (
@@ -7,6 +7,9 @@ export default function Home() {
       <Htag tag="h1">Htag пример</Htag>
       <Button appearance="primary" arrow="right">Привет</Button>
       <Button appearance="ghost" arrow="down">Привет ghost</Button>
+      <P size="l">Большой</P>
+      <P>Средней</P>
+      <P size="s">Маленький</P>
     </div>
   );
 }
